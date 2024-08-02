b5d94217076ff50af27515248838d25ffbd814cd
= <mridultiwari2002@gmail.com>
2024-08-02 12:08:40 +0530

    HTTPS STATUS CODE

---
+++ mridul/__init__.py
@@ -0,0 +1,2 @@
+from flask import Blueprint
+mridul=Blueprint("mridul",__name__,url_prefix="/api/v1/mridul")
---
+++ priyanka/__init__.py
@@ -0,0 +1,2 @@
+from flask import Blueprint
+priyanka=Blueprint("priyanka",__name__,url_prefix="/api/v1/priyanka")
